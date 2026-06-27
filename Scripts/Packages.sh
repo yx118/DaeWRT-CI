f6cf125e9019e22b5783c212d1266815249cd3a1
w_gaodawei <davidtall@qq.com>
2026-06-27 14:19:12 +0800

    Revert replace qca-qssdk

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -130,7 +130,6 @@ UPDATE_VERSION() {
 #删除官方的默认插件
 rm -rf ../feeds/luci/applications/luci-app-{passwall*,mosdns,dockerman,dae*,bypass*}
 rm -rf ../feeds/packages/net/{v2ray-geodata,dae*}
-rm -rf ../package/qca-nss/qca-ssdk
 cp -r $GITHUB_WORKSPACE/package/* ./
 #修复daed/Makefile
 #rm -rf luci-app-daed/daed/Makefile && cp -r $GITHUB_WORKSPACE/patches/daed/Makefile luci-app-daed/daed/
